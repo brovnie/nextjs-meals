8265e76669da1695fb7d8a6e16d4ecae0eca33ad
brovnie <bronka1.dev@gmail.com>
2024-10-23 11:25:17 +0200

    Added meal item

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import styles from './page.module.css';
+import Link from 'next/link';
+import MealsGrid from '@/components/meals/meals-grid';
 
 const MealsPage = () => {
-  return <div>MealsPage</div>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicionus meals created
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>Choose your favoriete recipe and cook it yourself! </p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share your favoriete recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
